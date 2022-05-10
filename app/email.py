e2db2e39b9667151990bb0f1010eb9bcc7c989a0
ann-wangui <wanguia822@gmail.com>
2022-05-10 22:21:00 +0300

    add all changes

---
+++ app/email.py
@@ -0,0 +1,11 @@
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+def mail_message(subject, template, to, **kwargs):
+    sender_mail = 'pitchesapp@gmail.com'
+
+    email = Message(subject, sender = sender_mail, recipients = [to])
+    email_body = render_template(template + '.txt', **kwargs)
+    email.html = render_template(template + '.html', **kwargs)
+    # mail.send(email)
