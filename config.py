78e9903244da22dc7208ecb9219843144ae8130c
ann-wangui <wanguia822@gmail.com>
2022-05-13 08:25:49 +0300

    add modified file changes

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config:
+
+    SECRET_KEY = '12673'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:ann-wangui#@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+   
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+
+class DevConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:ann-wangui@localhost/pitch'
+    DEBUG = True
